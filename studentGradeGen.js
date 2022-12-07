360f3bcdbc5fdaf0bf0bca5295e6892b9457caf5
Maureen N <monderebamo@gmail.com>
2022-12-07 12:03:12 +0300

    Added Student Grade Generator function

---
+++ studentGradeGen.js
@@ -0,0 +1,18 @@
+function studentGradeGenerator(x) {
+    if (x > 79) {
+      console.log("A")
+    }
+    else if (x >= 60) {
+      console.log("B")
+    }
+    else if (x > 49) {
+      console.log("C")
+    }
+    else if (x >= 40) {
+      console.log("D")
+    }
+    else {console.log("E")}
+  }
+  
+  studentGradeGenerator(68)
+  
